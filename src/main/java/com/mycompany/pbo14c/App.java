083c98525ca8969e43312c203221dae697905bde
mirzayogy <mirza.yogy@gmail.com>
2026-06-29 13:17:32 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/pbo14c/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo14c;
+
+import com.mycompany.pbo14c.frame.PenerbitTampilFrame;
+
+/**
+ *
+ * @author mirzayogy
+ */
+public class App {
+
+    public static void main(String[] args) {
+        PenerbitTampilFrame penerbitTampilFrame = new PenerbitTampilFrame();
+    }
+}
---
+++ src/main/java/com/mycompany/pbo14c/db/Koneksi.java
@@ -0,0 +1,31 @@
+package com.mycompany.pbo14c.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    private final String URL = "jdbc:mysql://localhost:3306/db_perpus";
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public Connection getConnection(){
+        Connection con = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Koneksi berhasil");
+            return con;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println("Error");
+            System.err.println(ex.toString());
+            return null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Koneksi koneksi = new Koneksi();
+        koneksi.getConnection();
+    }
+}
